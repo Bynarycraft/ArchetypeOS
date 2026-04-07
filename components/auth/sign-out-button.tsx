b7477c4f51cc96c08eb3138bf2a708a3e3bfb4ad
Excel Chiemeke <chiemekeexcel@gmail.com>
2026-04-07 06:50:41 +0100

    Fix React import for mobile sign-out component

--- components/auth/sign-out-button.tsx
+++ components/auth/sign-out-button.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import { signOut } from "next-auth/react";
 import { LogOut } from "lucide-react";
 import { Button } from "@/components/ui/button";
